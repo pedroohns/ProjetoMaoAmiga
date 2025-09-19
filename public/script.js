e744e1c019616d5111c489ee6d4e5fbc4a66d5d7
Pedro Henrique de Souza Silva <iampedro26@gmail.com>
2025-09-19 16:41:57 -0300

    esqueci de fazer o javascript para os botoes do menu hamburger funcionarem, agora esta feito.

--- public/script.js
+++ public/script.js
@@ -16,4 +16,21 @@ document.addEventListener('DOMContentLoaded', function() {
       window.location.href = 'tela-criar-conta.html';
     });
   }
+
+  //codigo pro entrar do menu hamburger
+  const btnMobileEntrar = document.querySelector('.mobile-entrar');
+  if (btnMobileEntrar) {
+    btnMobileEntrar.addEventListener('click', function() {
+      window.location.href = 'tela-login.html';
+    });
+  }
+
+  //codigo do criar conta pro menu hamburger
+  const btnMobileCriarConta = document.querySelector('.mobile-criar-conta');
+  if (btnMobileCriarConta) {
+    btnMobileCriarConta.addEventListener('click', function() {
+      window.location.href = 'tela-criar-conta.html';
+    });
+  }
+
 });
